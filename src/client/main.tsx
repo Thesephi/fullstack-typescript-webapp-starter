825dacfe142aa05c56a3491d29b0d8a1c6ae76f9
Khang Dinh <thesephi@gmail.com>
2020-11-30 08:46:40 +0100

    upgraded exp. React libs; improved hot reload handling

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,16 +1,24 @@
 import * as React from "react";
-import { unstable_createRoot } from "react-dom";
+import { Root, unstable_createRoot } from "react-dom";
 import App from "./App";
 
-unstable_createRoot(document.getElementById("main")!).render(<App />);
+const root: Root = unstable_createRoot(document.getElementById("main")!);
+root.render(<App />);
 
 declare var module: {
     hot?: any;
 }
+
 if (module.hot) {
-    // @TODO make current flow work with latest libraries, such that this error is no longer a concern:
-    // Warning: You are calling ReactDOM.createRoot() on a container that has already been passed to createRoot() before. Instead, call root.render() on the existing root instead if you want to update it.
-    module.hot.accept((err: unknown) => {
+    module.hot.accept("./App", (err: unknown) => {
+        try {
+            const NextApp = require("./App").default;
+            root.render(<NextApp />);
+            return;
+        } catch (e) {
+            console.error(e);
+            // to be reloaded
+        }
         console.error(err);
         console.log("reloading self");
         window.location.reload();
