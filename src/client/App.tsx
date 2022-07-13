275adaa74e4b2aa3f866054be114d27c4cf9364f
Khang Dinh <2597375+Thesephi@users.noreply.github.com>
2022-07-13 00:06:30 +0000

    upgraded to @mui/material

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { useState } from "react";
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Input from "@material-ui/core/Input";
-import { Alert, AlertTitle } from "@material-ui/lab";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Input from "@mui/material/Input";
+import { Alert, AlertTitle } from "@mui/material";
 import "./App.scss";
 
 import { isValidEmail } from "shared/utils";
